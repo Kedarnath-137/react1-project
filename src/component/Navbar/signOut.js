1c91fb5990e10d6e99835c85d3c83f0d37d1bd76
Kedarnath-137 <akedarnath137@gmail.com>
2024-06-25 11:24:28 +0530

    changes3

--- src/component/Navbar/signOut.js
+++ src/component/Navbar/signOut.js
@@ -4,7 +4,7 @@ import React from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "./firebase";
 import { useNavigate } from "react-router-dom";
-import './SignOut.css'
+import './signOut.css'
 
 function SignOut(){
     const navigate = useNavigate()
